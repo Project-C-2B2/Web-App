3be18da27deed81f8f6e3d2296236f43758d4559
Roby <Roby010@outlook.com>
2020-10-22 12:14:39 +0200

    RIP XAMPP

--- src/AppBundle/Controller/EmployeeController.php
+++ src/AppBundle/Controller/EmployeeController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class EmployeeController extends Controller
 {
@@ -26,6 +27,19 @@ class EmployeeController extends Controller
         // replace this example code with whatever you need
         return $this->render('employeeLogin/login.html.twig', [
             'msg' => 'here'
+        ]);
+    }
+    public function loginAction(AuthenticationUtils $authenticationUtils)
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
         ]);
     }
 }
