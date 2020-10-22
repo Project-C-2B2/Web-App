626d7106e0c4b42e2acc269a30fa6f157d6305a9
Tobias <Tobizelders@gmail.com>
2020-10-22 11:19:58 +0200

    Fixed Photo and Navbar
    Na struggel eindelijk gefixt

--- src/AppBundle/Controller/EmployeeController.php
+++ src/AppBundle/Controller/EmployeeController.php
@@ -9,12 +9,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class EmployeeController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/User/Dashboard", name="User-Dashboard")
      */
     public function indexAction(Request $request)
     {
+        dump(ini_get('memory_limit'));
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('User-Dashboard/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
