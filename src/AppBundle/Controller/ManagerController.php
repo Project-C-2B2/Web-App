d1dc33ec87ebbedd4f0664bbfe680cb9ce8a4663
tayfun <0972251@hr.nl>
2020-10-22 13:31:31 +0200

    GuestPage

--- src/AppBundle/Controller/ManagerController.php
+++ src/AppBundle/Controller/ManagerController.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace AppBundle\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
