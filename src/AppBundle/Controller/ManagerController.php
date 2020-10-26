a346be882b0855469dd0bcbb79073b98d67b392d
tayfun <0972251@hr.nl>
2020-10-26 10:12:04 +0100

    gastpagina
    moet nog wat tekst komen om meer informatie te geven over het gebruik

--- src/AppBundle/Controller/ManagerController.php
+++ src/AppBundle/Controller/ManagerController.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace AppBundle\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
