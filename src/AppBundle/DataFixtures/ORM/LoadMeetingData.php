86e79ca3d894b5f7dbd471cfee6f9018bd94c159
SvenBrandt <svenfire99@gmail.com>
2021-01-11 16:58:34 +0100

    Tijd

--- src/AppBundle/DataFixtures/ORM/LoadMeetingData.php
+++ src/AppBundle/DataFixtures/ORM/LoadMeetingData.php
@@ -54,6 +54,38 @@ class LoadMeetingData extends Fixture implements DependentFixtureInterface
         }
         $manager->persist($meeting);
         $manager->flush();
+
+        $meeting = new Meeting();
+        $meeting->setName('Weekelijkse Meeting');
+        $meeting->setDescription('Deze meeting is voor de stress');
+        $dateTime = new \DateTime(date('Y-m-d H:i:s',strtotime('+2 day',strtotime(date("Y-m-d H:i:s")))));
+        $meeting->setDateTime($dateTime);
+        $meeting->setLocation('Online');
+        $meeting->setGroup($manager->getRepository(Group::class)->findOneByName("Locatie: Berlijn"));
+
+        foreach ($this->meetingManager->getUsersByGroup($meeting->getGroup()) as $user) {
+            $meetingInUserAssoc = new MeetingsInUserAssociation($user->getUser(), $meeting);
+            $meeting->addMeetingsInUserAssociation($meetingInUserAssoc);
+            $manager->persist($meetingInUserAssoc);
+        }
+        $manager->persist($meeting);
+        $manager->flush();
+
+        $meeting = new Meeting();
+        $meeting->setName('Maandelijkse Meeting');
+        $meeting->setDescription('Deze meeting is voor de stress');
+        $dateTime = new \DateTime(date('Y-m-d H:i:s',strtotime('+2 day',strtotime(date("Y-m-d H:i:s")))));
+        $meeting->setDateTime($dateTime);
+        $meeting->setLocation('Online');
+        $meeting->setGroup($manager->getRepository(Group::class)->findOneByName("Locatie: Amsterdam"));
+
+        foreach ($this->meetingManager->getUsersByGroup($meeting->getGroup()) as $user) {
+            $meetingInUserAssoc = new MeetingsInUserAssociation($user->getUser(), $meeting);
+            $meeting->addMeetingsInUserAssociation($meetingInUserAssoc);
+            $manager->persist($meetingInUserAssoc);
+        }
+        $manager->persist($meeting);
+        $manager->flush();
     }
 
     public function getDependencies()
